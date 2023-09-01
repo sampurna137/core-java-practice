0f0056412404dcf78de327495d7892e2d9530351
Sampurna Shrestha <sshrestha11@cub.uca.edu>
2023-09-01 18:09:36 -0500

    added three java class scenarios

---
+++ src/UberRide.java
@@ -0,0 +1,42 @@
+public class UberRide {
+    //States or instance variables
+    /*
+        driver name
+        driver vehicle type
+        driver vehicle number
+        driver vehicle description
+        driver profile picture
+        driver location
+        driver rating
+
+        customer name
+        customer password
+        customer payment information
+        customer profile picture
+        customer location
+        customer rating
+
+        estimated arrival time
+        ride completion status
+
+     */
+
+
+    // Constructors
+
+
+    // Behaviors or functions or methods
+
+    /*
+        search ride
+        order ride
+        cancel ride
+
+        ride payment
+        add tip
+        add driver rating
+        add customer rating
+
+
+     */
+}
---
+++ src/YoutubeVideo.java
@@ -0,0 +1,60 @@
+public class YoutubeVideo {
+    //States or instance variables
+    /*
+        video title
+        video url
+        video description
+        video thumbnail
+        video quality
+        video likes
+        video dislikes
+        video creation date
+        video views
+        video comments
+        video share link
+        video download url
+
+        channel name
+        channel subscribers
+        channel profile picture
+
+
+     */
+
+
+    // Constructors
+
+
+    // Behaviors or functions or methods
+
+    /*
+        add video title
+        add video url
+        add video description
+        add video thumbnail
+
+        post video
+        edit video information
+        remove video
+        set visibility (private / public)
+
+        like video
+        dislike video
+        share video link
+        download video
+        save video
+        report video
+
+        add comment
+        edit comment
+        remove comment
+        subscribe to channel
+
+
+
+
+
+
+
+     */
+}
